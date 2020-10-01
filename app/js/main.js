92baad49efc8be21770ca5859281e490d3a9b66e
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-10-01 11:15:39 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function () {
-    var mixer = mixitup('.products__box'); 
+
+    $(".rate-star").rateYo({
+        rating: 4,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+    var mixer = mixitup('.products__box');
+
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
