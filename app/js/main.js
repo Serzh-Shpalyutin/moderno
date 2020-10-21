40dc5ca4d66a4647416327dae9dedb6045139739
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-10-21 16:17:40 +0300

    added new-items.html

--- app/js/main.js
+++ app/js/main.js
@@ -4,9 +4,7 @@ $(function () {
         rating: 4,
         starWidth: "12px",
         readOnly: true
-      });
-
-    var mixer = mixitup('.products__box');
+    });
 
     $('.product-slider__inner').slick({
         dots: true,
@@ -14,4 +12,17 @@ $(function () {
         slidesToShow: 4,
         slidesToScroll: 4
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    });
+
+    var mixer = mixitup('.products__box');
+
+
 });
