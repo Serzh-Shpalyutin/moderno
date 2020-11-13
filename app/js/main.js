ae9971b15334934229295f0fbf6de93ec9a708d5
Serzh-Shpalyutin <serzh.shpalyutin@mail.ru>
2020-11-13 11:25:25 +0300

    added settings.html

--- app/js/main.js
+++ app/js/main.js
@@ -43,10 +43,10 @@ $(function () {
         $('.icon-th-list').removeClass('active');
     });
 
-    $('.product-one__tabs .tab').on('click', function (event) {
+    $('.product-one__tabs .tab, .settings__tabs .tabs').on('click', function (event) {
         var id = $(this).attr('data-id');
-        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-        $('.product-one__tabs .tabs').find('.tab').removeClass('tab--active');
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('tab--active');
         $(this).addClass('tab--active');
         $('#' + id).addClass('active-tab').fadeIn();
         return false;
